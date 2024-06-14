c8948613fc97999c836a5044cb25f5db8474d993
azim0nt <kktotov457@gmail.com>
2024-06-14 17:00:38 +0500

    create path to tab component

--- frontend/src/components/Dashboards/Default/index.jsx
+++ frontend/src/components/Dashboards/Default/index.jsx
@@ -1,10 +1,22 @@
 import './style.scss'
+import PathToTab from '../../common/PathToTab';
+
 function Default() {
-    return ( 
+    return (
         <>
-        <h1>Default</h1>
+            <div className="default-wrapper">
+                <div className="default-content">
+                    <div className="top">
+                        <h3>Default Dashboard</h3>
+                    <PathToTab parent={'Dashboards'} tab={'Default'} />
+                    </div>
+                    <div className="middle">
+                        
+                    </div>
+                </div>
+            </div>
         </>
-     );
+    );
 }
 
 export default Default;
---
+++ frontend/src/components/common/PathToTab.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import './PathToTab.scss'
+function PathToTab(props) {
+  return (
+    <>
+      <div className="path-to-tab-wrapper">
+        <div className="path-to-tab-content">
+          <span><Link to={'/index'}><FaHome color="#5c61f2" size={20} /></Link> / <span id="parent">{props.parent} </span>/ <span id="tab">{props.tab}</span></span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PathToTab;
