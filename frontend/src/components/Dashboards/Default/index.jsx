38616eb7b788aee6b4c018552b1c44183a0b29f7
azim0nt <kktotov457@gmail.com>
2024-06-19 22:59:36 +0500

    create orders table component

--- frontend/src/components/Dashboards/Default/index.jsx
+++ frontend/src/components/Dashboards/Default/index.jsx
@@ -19,6 +19,7 @@ import User2 from '../../../assets/images/users/2.png'
 import User3 from '../../../assets/images/users/3.png'
 import User4 from '../../../assets/images/users/4.png'
 
+import OrdersTable from '../../common/OrdersTable'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, elements);
 
@@ -100,61 +101,7 @@ function Default() {
             },
         },
     };
-    const [data, setData] = useState(userData);
-    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
-    const [searchQuery, setSearchQuery] = useState('');
-
-    // Функция для обработки сортировки
-    const sortData = (key) => {
-        let direction = 'asc';
-        if (sortConfig.key === key && sortConfig.direction === 'asc') {
-            direction = 'desc';
-        }
-
-        const sortedData = [...data].sort((a, b) => {
-            let aValue = a[key];
-            let bValue = b[key];
-
-            if (key === 'date') {
-                aValue = formatDate(aValue);
-                bValue = formatDate(bValue);
-            } else if (key === 'amount') {
-                aValue = parseFloat(aValue.replace(/[$,]/g, ''));
-                bValue = parseFloat(bValue.replace(/[$,]/g, ''));
-            } else if (key === 'id') {
-                aValue = parseInt(aValue);
-                bValue = parseInt(bValue);
-            }
-
-            if (aValue < bValue) {
-                return direction === 'asc' ? -1 : 1;
-            }
-            if (aValue > bValue) {
-                return direction === 'asc' ? 1 : -1;
-            }
-            return 0;
-        });
-
-        setData(sortedData);
-        setSortConfig({ key, direction });
-    };
-
-    // Функция для форматирования даты
-    const formatDate = (dateString) => {
-        const [day, month, year] = dateString.split('/');
-        return new Date(`${year}-${month}-${day}`);
-    };
-
-    // Функция для фильтрации данных
-    const filteredData = data.filter(user => user.fullname.toLowerCase().includes(searchQuery.toLowerCase()));
-    const [checkedState, setCheckedState] = useState({});
-
-    const handleCheckboxChange = (userId) => {
-        setCheckedState(prevState => ({
-            ...prevState,
-            [userId]: !prevState[userId]
-        }));
-    };
+    
     return (
         <>
             <div className="default-wrapper">
@@ -238,45 +185,7 @@ function Default() {
                         </div>
                         <div className="section-2">
                             <div className="recent-order-card">
-                                <div className="top-part">
-                                    <h4>Recent Order</h4>
-                                    <div className="input">
-                                        <p>Search: <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="cards">
-                                    <table>
-                                        <thead>
-                                            <tr>
-                                                <th><input type="checkbox" /></th>
-                                                <th><button onClick={() => sortData('index')}>No</button>.</th>
-                                                <th><button onClick={() => sortData('fullname')}>CustomerName</button></th>
-                                                <th><button onClick={() => sortData('id')}>ID</button></th>
-                                                <th><button onClick={() => sortData('date')}>Date</button></th>
-                                                <th><button onClick={() => sortData('city')}>City</button></th>
-                                                <th><button onClick={() => sortData('status')}>Status</button></th>
-                                                <th><button onClick={() => sortData('amount')}>Amount</button></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {filteredData.map((user, index) => (
-                                                <tr key={index}>
-                                                    <td><input type="checkbox"
-                                                        checked={checkedState[user.index ] || false}
-                                                        onChange={() => handleCheckboxChange(user.index)} /></td>
-                                                    <td><p>{user.index}</p></td>
-                                                    <td className='avatar-fullname'><img src={usersImages[user.index - 1]} alt="" /><p>{user.fullname}</p></td>
-                                                    <td><p>#{user.id}</p></td>
-                                                    <td className='date'><p>{user.date}</p></td>
-                                                    <td><p>{user.city}</p></td>
-                                                    <td><p className={`${user.status.toLowerCase()}`}>{user.status}</p></td>
-                                                    <td><p>${user.amount}</p></td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
+                               <OrdersTable data={userData} title={'R'} images={usersImages}/>
                             </div>
                             <div className="our-growth-card">
                                 <h4>
