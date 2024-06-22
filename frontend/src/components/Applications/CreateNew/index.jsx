fe621b59fe093cf542d8c16fca55e3ee11ba4e32
azim0nt <kktotov457@gmail.com>
2024-06-22 23:01:05 +0500

    create new project component

--- frontend/src/components/Applications/CreateNew/index.jsx
+++ frontend/src/components/Applications/CreateNew/index.jsx
@@ -1,11 +1,82 @@
 import './style.scss'
+import '../../common/PathToTab'
+import { useContext ,useState } from 'react';
+import { context } from '../../../store'
+import PathToTab from '../../common/PathToTab';
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 function CreateNew() {
-    return ( 
+    const { store, setStore } = useContext(context)
+    const [startDate, setStartDate] = useState(new Date());
+    return (
         <>
-        <h1>CreateNew</h1>
+            <div className="create-new-wrapper" style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }}>
+                <div className="create-new-content">
+                    <div className="top">
+                        <h3>Project Create</h3>
+                        <PathToTab parent={'Apps'} tab={'Project Create'} />
+                    </div>
+                    <div className="middle">
+                        <div className="form-card" style={{ backgroundColor: store.theme.bgColor, color: store.theme.textColor }}>
+                            <div className="project-title card">
+                                <h4>Project Title</h4>
+                                <input type="text" placeholder='Project name' style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }} />
+                            </div>
+                            <div className="client-name card">
+                                <h4>Client name</h4>
+                                <input type="text" placeholder='Name client or company name' style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }} />
+                            </div>
+                            <div className="three-inputs">
+                                <div className="project-rate">
+                                    <h4>Project Rate</h4>
+                                    <input type="text" placeholder='Enter project Rate' style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }} />
+                                </div>
+                                <div className="project-type">
+                                    <h4>Project Type</h4>
+                                    <select style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }} >
+                                        <option value="hourly">Hourly</option>
+                                        <option value="fix-price">Fix price</option>
+                                    </select>
+                                </div>
+                                <div className="priority">
+                                    <h4>Priority</h4>
+                                    <select  style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }} >
+                                        <option value="low">Low</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="high">High</option>
+                                        <option value="urgent">Urgent</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="date">
+                                <div className="project-size">
+                                    <h3>Project Size</h3>
+                                    <select  style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }} >
+                                        <option value="small">Small</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="big">Big</option>
+                                    </select>
+                                </div>
+                                <div className="starting-date">
+                                    <h3>Starting date</h3>
+                                    <div id='datepick'>
+                                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                                    </div>
+                                </div>
+                                <div className="starting-date">
+                                    <h3>Starting date</h3>
+                                    <div id='datepick'>
+                                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
-     );
+    );
 }
 
 export default CreateNew;
--- frontend/src/components/CurrentPage/index.jsx
+++ frontend/src/components/CurrentPage/index.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 import { Outlet} from 'react-router-dom';
 import Header from '../Header';
 import General from '../General';
-
+import { useContext } from 'react';
+import { context } from '../../store';
 function CurrentPage() {
+    const {store, setStore} = useContext(context);
     return (
         <div className="body">
             {/* <Redirect to="/login" /> */}
@@ -15,6 +17,11 @@ function CurrentPage() {
             <div className="right-panel">
                 <Header />
                 <Outlet />
+                <div className="footer" style={{backgroundColor:store.theme.bgColor, color:store.theme.textColor}}>
+                    <p>
+                Copyright 2024 © Crocs theme by pixelstrap.
+                    </p>
+                </div>
             </div>
         </div>
     );
