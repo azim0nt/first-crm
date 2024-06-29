2986101d20bfd8ccc73a1cf2a3414af4b8c4bc17
azim0nt <kktotov457@gmail.com>
2024-06-29 13:58:29 +0500

    create payment details

--- frontend/src/components/Applications/OrderHistory/index.jsx
+++ frontend/src/components/Applications/OrderHistory/index.jsx
@@ -1,22 +1,205 @@
 import './style.scss'
-import { useContext } from 'react';
-import {context} from '../../../store'
+import { useContext, useState } from 'react';
+import { context } from '../../../store'
 import PathToTab from '../../common/PathToTab'
+import User1 from '../../../assets/images/users-3/image_1.jpg'
+import User2 from '../../../assets/images/users-3/image_2.jpg'
+import User3 from '../../../assets/images/users-3/image_3.jpg'
+import User4 from '../../../assets/images/users-3/image_4.jpg'
+import User5 from '../../../assets/images/users-3/image_5.jpg'
+import User6 from '../../../assets/images/users-3/image_6.jpg'
+import User7 from '../../../assets/images/users-3/image_7.jpg'
+import User8 from '../../../assets/images/users-3/image_8.jpg'
+import User9 from '../../../assets/images/users-3/image_9.jpg'
+import User10 from '../../../assets/images/users-3/image_10.jpg'
+import User11 from '../../../assets/images/users-3/image_11.jpg'
+import User12 from '../../../assets/images/users-3/image_12.jpg'
+import User13 from '../../../assets/images/users-3/image_13.jpg'
+import User14 from '../../../assets/images/users-3/image_14.jpg'
+import User15 from '../../../assets/images/users-3/image_15.jpg'
+import { FaStar } from "react-icons/fa";
+import { ImCancelCircle } from "react-icons/im";
 function OrderHistory() {
-    const {store, setStore} = useContext(context)
-    const cardStyle = {backgroundColor:store.theme.bgcolor}
-    return ( 
+    const { store, setStore } = useContext(context)
+    const cardStyle = { backgroundColor: store.theme.bgColor }
+
+
+    const [images1, setImages1] = useState([User1, User2, User3, User4, User5, User6, User7, User8, User9]);
+    const [images2, setImages2] = useState([User1, User2, User3, User4, User5, User6, User7, User8, User9]);
+    const [images3, setImages3] = useState([User1, User2, User3, User4, User5, User6, User7, User8, User9]);
+
+    const removeCard1 = (index) => {
+        const newImages = [...images1];
+        newImages.splice(index, 1);
+        setImages1(newImages);
+    };
+
+    const removeCard2 = (index) => {
+        const newImages = [...images2];
+        newImages.splice(index, 1);
+        setImages2(newImages);
+    };
+
+    const removeCard3 = (index) => {
+        const newImages = [...images3];
+        newImages.splice(index, 1);
+        setImages3(newImages);
+    };
+
+    return (
         <>
-        <div className="order-history-wrapper" style={{backgroundColor:store.theme.backBgColor, color:store.theme.textColor}}>
-            <div className="order-history-content">
-                <div className="top">
-                    <h3>Order History</h3>
-                    <PathToTab parent={'ECommerce'} tab={'Order History'}/>
+            <div className="order-history-wrapper" style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }}>
+                <div className="order-history-content">
+                    <div className="top">
+                        <h3>Recent History</h3>
+                        <PathToTab parent={'ECommerce'} tab={'Order History'} />
+                    </div>
+                    <div className="middle">
+                        <div className="section-1">
+                            <div className="new-orders-card" style={cardStyle}>
+                                <h4>New Orders</h4>
+                                <div className="cards">
+                                    {
+                                        images1.map((image, i) => {
+                                            return (
+                                                <div className="cardd" key={i} style={cardStyle}>
+                                                    <div className="left-part">
+                                                        <img src={image} alt="" />
+                                                    </div>
+                                                    <div className="middle-part">
+                                                        <p className="product">
+                                                            Women Top
+                                                        </p>
+                                                        <div className="stars">
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                        </div>
+                                                        <p className="price">
+                                                            Price : 210$
+                                                        </p>
+                                                        <p className="stock">
+                                                            In stock
+                                                        </p>
+                                                    </div>
+                                                    <div className="right-part">
+                                                        <div>
+                                                            <button  onClick={() => removeCard1(i)}>
+                                                                <ImCancelCircle color={store.theme.textColor}/>
+                                                            </button>
+                                                        </div>
+                                                        <div>
+                                                            <button className="blue-btn" >
+                                                                Move to Cart
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            <div className="new-orders-card" style={cardStyle}>
+                                <h4>New Orders</h4>
+                                <div className="cards">
+                                    {
+                                        images2.map((image, i) => {
+                                            return (
+                                                <div className="cardd" key={i} style={cardStyle}>
+                                                    <div className="left-part">
+                                                        <img src={image} alt="" />
+                                                    </div>
+                                                    <div className="middle-part">
+                                                        <p className="product">
+                                                            Women Top
+                                                        </p>
+                                                        <div className="stars">
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                        </div>
+                                                        <p className="price">
+                                                            Price : 210$
+                                                        </p>
+                                                        <p className="stock">
+                                                            In stock
+                                                        </p>
+                                                    </div>
+                                                    <div className="right-part">
+                                                        <div>
+                                                            <button  onClick={() => removeCard2(i)}>
+                                                                <ImCancelCircle color={store.theme.textColor}/>
+                                                            </button>
+                                                        </div>
+                                                        <div>
+                                                            <button className="blue-btn" >
+                                                                Move to Cart
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            <div className="new-orders-card" style={cardStyle}>
+                                <h4>New Orders</h4>
+                                <div className="cards">
+                                    {
+                                        images3.map((image, i) => {
+                                            return (
+                                                <div className="cardd" key={i} style={cardStyle}>
+                                                    <div className="left-part">
+                                                        <img src={image} alt="" />
+                                                    </div>
+                                                    <div className="middle-part">
+                                                        <p className="product">
+                                                            Women Top
+                                                        </p>
+                                                        <div className="stars">
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                            <FaStar color='#FFAF45' />
+                                                        </div>
+                                                        <p className="price">
+                                                            Price : 210$
+                                                        </p>
+                                                        <p className="stock">
+                                                            In stock
+                                                        </p>
+                                                    </div>
+                                                    <div className="right-part">
+                                                        <div>
+                                                            <button  onClick={() => removeCard3(i)}>
+                                                                <ImCancelCircle color={store.theme.textColor}/>
+                                                            </button>
+                                                        </div>
+                                                        <div>
+                                                            <button className="blue-btn" >
+                                                                Move to Cart
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
-     );
+    );
 }
 
 export default OrderHistory;
--- frontend/src/components/Applications/PaymentDetails/index.jsx
+++ frontend/src/components/Applications/PaymentDetails/index.jsx
@@ -1,22 +1,174 @@
 import './style.scss'
 import { useContext } from 'react';
-import {context} from '../../../store'
+import { context } from '../../../store'
 import PathToTab from '../../common/PathToTab'
+import Card from '../../../assets/images/card.png'
+import Visa from '../../../assets/images/visa.png'
+import MasterCard from '../../../assets/images/mastercard.png'
+import PayPal from '../../../assets/images/paypal_2.png'
 function PaymentDetails() {
-    const {store, setStore} = useContext(context)
-    const cardStyle = {backgroundColor:store.theme.bgcolor}
-    return ( 
+    const { store, setStore } = useContext(context)
+    const cardStyle = { backgroundColor: store.theme.bgColor }
+    const inputStyle = { backgroundColor: store.theme.backBgColor, border: '1px solid #88888840', outline: 'none', width: '100%', padding: '10px 20px', borderRadius: '5px', color: store.theme.textColor }
+
+    return (
         <>
-        <div className="payment-details-wrapper" style={{backgroundColor:store.theme.backBgColor, color:store.theme.textColor}}>
-            <div className="payment-details-content">
-                <div className="top">
-                    <h3>Payment Details</h3>
-                    <PathToTab parent={'ECommerce'} tab={'PaymentDetails'}/>
+            <div className="payment-details-wrapper" style={{ backgroundColor: store.theme.backBgColor, color: store.theme.textColor }}>
+                <div className="payment-details-content">
+                    <div className="top">
+                        <h3>Payment Details</h3>
+                        <PathToTab parent={'ECommerce'} tab={'PaymentDetails'} />
+                    </div>
+                    <div className="middle">
+                        <div className="section-1">
+                            <div className="credit-card" style={cardStyle}>
+                                <div className="left">
+                                    <h4>Credit card</h4>
+                                    <div className="inputs">
+                                        <input type="text" style={inputStyle} placeholder='Card Number' />
+                                        <input type="text" style={inputStyle} placeholder='First Name' />
+                                        <input type="date" style={inputStyle} />
+                                        <input type="text" style={inputStyle} placeholder='Name on card' />
+                                        <select name="" id="" style={inputStyle}>
+                                            <option value="select_type">Select Type</option>
+                                            <option value="master">Master</option>
+                                            <option value="visa">Visa</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="right">
+                                    <img src={Card} alt="" />
+                                </div>
+                            </div>
+                            <div className="debit-card" style={cardStyle}>
+                                <h4>Debit card</h4>
+                                <div className="fullname-card">
+                                    <input type="text" style={inputStyle} placeholder='Full name here' />
+                                    <input type="text" style={inputStyle} placeholder='Card number' />
+                                </div>
+                                <div className="cvv-cvc-number">
+                                    <input type="text" style={inputStyle} placeholder='CVV number' />
+                                    <input type="text" style={inputStyle} placeholder='CVC' />
+                                </div>
+                                <h4>Exripation Date</h4>
+                                <div className="select-year-month">
+                                    <select name="" id="" style={inputStyle}>
+                                        <option value="select_moth">Select Month</option>
+                                        <option value="jan">Jan</option>
+                                        <option value="fab">Fab</option>
+                                        <option value="march">March</option>
+                                        <option value="april">April</option>
+                                    </select>
+                                    <select name="" id="" style={inputStyle}>
+                                        <option value="select_year">Select Year</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2023">2023</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <button className="blue-btn">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section-2">
+                            <div className="cod-card" style={cardStyle}>
+                                <h4>COD</h4>
+                                <div className="first-last-name">
+                                    <input type="text" style={inputStyle} placeholder='First name' />
+                                    <input type="text" style={inputStyle} placeholder='Last name' />
+                                </div>
+                                <div className="pin-mobile-number">
+                                    <input type="text" style={inputStyle} placeholder='Pincode' />
+                                    <input type="text" style={inputStyle} placeholder='Enter mobile number' />
+                                </div>
+                                <div className="state-city">
+                                    <input type="text" style={inputStyle} placeholder='State' />
+                                    <input type="text" style={inputStyle} placeholder='City' />
+                                </div>
+                                <textarea  name="" id="" style={inputStyle} placeholder='Address'>
+
+                                </textarea>
+                                <div>
+                                    <button className="blue-btn">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="emi-card" style={cardStyle}>
+                                <h4>EMI</h4>
+                                <div className="first-last-name">
+                                    <input type="text" style={inputStyle} placeholder='First name' />
+                                    <input type="text" style={inputStyle} placeholder='Last name' />
+                                </div>
+                                <div className="pin-mobile-number">
+                                    <input type="text" style={inputStyle} placeholder='Pincode' />
+                                   <select name="" id="" style={inputStyle}>
+                                    <option value="bank_name">Bank Name</option>
+                                    <option value="sbi">SBI</option>
+                                    <option value="icici">ICICI</option>
+                                    <option value="kotak">KOTAK</option>
+                                    <option value="BOB">BOB</option>
+                                   </select>
+                                </div>
+                                    <select name="" id="" style={inputStyle}>
+                                        <option value="select_card">Select Card</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                    <select name="" id="" style={inputStyle}>
+                                        <option value="select_card">Select Duration</option>
+                                        <option value="2023-2023">2023-2023</option>
+                                        <option value="2023-2023">2023-2023</option>
+                                        <option value="2023-2023">2023-2023</option>
+                                        <option value="2023-2023">2023-2023</option>
+                                    </select>
+                                    <div className="cards-logo">
+                                        <div>
+                                            <img src={MasterCard} alt="" />
+                                        </div>
+                                        <div>
+                                            <img src={Visa} alt="" />
+                                        </div>
+                                        <div>
+                                            <img src={PayPal} alt="" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className="blue-btn">
+                                            Submit
+                                        </button>
+                                        </div>
+                            </div>
+                            <div className="net-banking" style={cardStyle}>
+                                <h4>Net Banking</h4>
+                                <input type="text" style={inputStyle} placeholder='AC Holder name' />
+                                <input type="text" style={inputStyle} placeholder='Account number' />
+                                <div className="bank-icfc-code">
+                                <select name="" id="" style={inputStyle}>
+                                    <option value="bank_name">Bank Name</option>
+                                    <option value="sbi">SBI</option>
+                                    <option value="icici">ICICI</option>
+                                    <option value="kotak">KOTAK</option>
+                                    <option value="BOB">BOB</option>
+                                   </select>
+                                   <input type="text" style={inputStyle} placeholder='ICFC code' />
+                                </div>
+                                <input type="text" style={inputStyle} placeholder='Enter mobile number'/>
+                                <input type="text" style={inputStyle} placeholder='Other Details'/>
+                                <div><button className="blue-btn">Submit</button></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
-     );
+    );
 }
 
 export default PaymentDetails;
