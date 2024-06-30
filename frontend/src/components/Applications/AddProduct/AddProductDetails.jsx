5b7660737d0f4fba752b5ba37c3fdaef1928f3f4
azim0nt <kktotov457@gmail.com>
2024-06-30 22:56:34 +0500

    create AddProduct component & create AppProductDetails for AddProduct component

---
+++ frontend/src/components/Applications/AddProduct/AddProductDetails.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.bubble.css';
+import { context } from '../../../store';
+
+function AddProductDetails() {
+    const [editorHtml, setEditorHtml] = useState('');
+    const {store, setStore} = useContext(context)
+    const theme = store.theme.textColor === '#171829' ? 'snow' : 'dark';
+    const modules = {
+        toolbar: [
+          [{ 'size': ['', 'small', 'large', 'huge'] }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ 'indent': '-1' }, { 'indent': '+1' },{ 'list': 'ordered' }, { 'list': 'bullet' }],
+          ['link', 'image', 'video'],
+
+        ],
+      };
+
+    return (
+        <>
+            <div className="add-product-details">
+                <div className="product-title">
+                    <h4>Product Title</h4>
+                    <input type="text" style={{border:`1px solid ${store.theme.textColor}`, color:store.theme.textColor}} />
+                </div>
+            <ReactQuill  value={editorHtml} modules={modules} className={theme}  onChange={setEditorHtml} />
+
+            </div>
+        </>
+    );
+}
+
+export default AddProductDetails;
--- frontend/src/components/Applications/AddProduct/index.jsx
+++ frontend/src/components/Applications/AddProduct/index.jsx
@@ -1,10 +1,23 @@
 import './style.scss'
-import { useContext } from 'react';
+import { useContext,useState } from 'react';
 import {context} from '../../../store'
 import PathToTab from '../../common/PathToTab'
+import ReactQuill from 'react-quill';
+import { MdAddchart } from "react-icons/md";
+import 'react-quill/dist/quill.snow.css';
+import { RiImageAddFill } from "react-icons/ri";
+import { FiLayers } from "react-icons/fi";
+import { IoIosPricetag } from "react-icons/io";
+import { FaHandHoldingDollar } from "react-icons/fa6";
+import AddProductDetails from './AddProductDetails';
 function AddProduct() {
     const {store, setStore} = useContext(context)
-    const cardStyle = {backgroundColor:store.theme.bgcolor}
+    const cardStyle = {backgroundColor:store.theme.bgColor}
+    const [activeComponent, setActiveComponent] = useState('AddProductDetails');
+
+    const handleComponentChange = (componentName) => {
+        setActiveComponent(componentName);
+      };
     return ( 
         <>
         <div className="add-product-wrapper" style={{backgroundColor:store.theme.backBgColor, color:store.theme.textColor}}>
@@ -12,6 +25,99 @@ function AddProduct() {
                 <div className="top">
                     <h3>Add-Product</h3>
                     <PathToTab parent={'ECommerce'} tab={'Add-Product'}/>
+                </div>
+                <div className="middle">
+                    <div className="section-1">
+                        <div className="product-form-card" style={cardStyle}>
+                            <h4>Product Form</h4>
+                            <div className="body">
+                                <div className="left">
+                                    <ul>
+                                        <li>
+                                            <div className="add-product-details btn">
+                                        <button  onClick={() => handleComponentChange('AddProductDetails')}>
+                                        <div className={`left-part ${activeComponent === 'AddProductDetails' ? 'active' : ''}`}>
+                                            <div>
+                                            <MdAddchart size={25} color={'#c9c9c950'}/>
+                                            </div>
+                                        </div>
+                                        <div className="right-part" style={{color:store.theme.textColor}}>
+                                            <b>Add Product Details</b>
+                                            <p>Add Product name & details</p>
+                                        </div>
+                                        </button>
+                                    </div>   
+                                        </li>
+                                        <li>
+                                            <div className="product-gallery btn">
+                                        <button>
+                                        <div className="left-part">
+                                            <div>
+                                            <RiImageAddFill size={25} color={'#c9c9c950'}/>
+                                            </div>
+                                        </div>
+                                        <div className="right-part" style={{color:store.theme.textColor}}>
+                                            <b>Product gallery</b>
+                                            <p>thumbnail & Add Product Gallery</p>
+                                        </div>
+                                        </button>
+                                    </div> 
+                                        </li>
+                                        <li>
+                                            <div className="product-gallery btn">
+                                        <button>
+                                        <div className="left-part">
+                                            <div>
+                                            <FiLayers size={25} color={'#c9c9c950'}/>
+                                            </div>
+                                        </div>
+                                        <div className="right-part" style={{color:store.theme.textColor}}>
+                                            <b>Product Categories</b>
+                                            <p>Add Product category, Status and Tags</p>
+                                        </div>
+                                        </button>
+                                    </div> 
+                                        </li>
+                                        <li>
+                                            <div className="product-gallery btn">
+                                        <button>
+                                        <div className="left-part">
+                                            <div>
+                                            <IoIosPricetag size={25} color={'#c9c9c950'}/>
+                                            </div>
+                                        </div>
+                                        <div className="right-part" style={{color:store.theme.textColor}}>
+                                            <b>Selling prices</b>
+                                            <p>Add Product basic price & Discount</p>
+                                        </div>
+                                        </button>
+                                    </div> 
+                                        </li>
+                                        <li>
+                                            <div className="product-gallery btn">
+                                        <button>
+                                        <div className="left-part">
+                                            <div>
+                                            <FaHandHoldingDollar size={25} color={'#c9c9c950'}/>
+                                            </div>
+                                        </div>
+                                        <div className="right-part" style={{color:store.theme.textColor}}>
+                                            <b>Advance</b>
+                                            <p>Add Meta details & Inventory details</p>
+                                        </div>
+                                        </button>
+                                    </div> 
+                                        </li>
+                                    </ul>
+                                 
+                                   
+                                </div>
+                                <div className="right">
+                                {activeComponent === 'AddProductDetails' && <AddProductDetails />}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
