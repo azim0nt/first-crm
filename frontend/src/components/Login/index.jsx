db68a1fae657ba83745d6c8aa23403b0c4aef2a2
azim0nt <kktotov457@gmail.com>
2024-07-23 08:29:39 +0500

    create login page

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,9 +1,22 @@
+import './style.scss'
+import Icon from '../../assets/images/aileet-icon.png';  
 function Login() {
   return ( 
     <>
     <div className="login-wrapper">
       <div className="login-content">
-        <h1>login</h1>
+        <div className="logo">
+          <img src={Icon} alt="" /><h1>Aileet</h1>
+          </div>
+          <div className="sign-in">
+            <form action="">
+            <label htmlFor='username'>Username</label>
+            <input type="text" name='username' placeholder='Username' />
+            <label htmlFor="passwd">Password</label>
+            <input type="password" name='passwd' placeholder='Password' />
+            <button type="submit">Sign In</button>
+            </form>
+        </div>
       </div>
       </div></>
    );
